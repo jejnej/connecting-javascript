34cef08b29cacdf8360763d77009adbb661e4b67
Jojo <jody.sekhon@gmail.com>
2018-04-03 17:25:58 +0000

    Minor formatting changes

--- lookup_people.js
+++ lookup_people.js
@@ -1,19 +1,19 @@
 const pg = require("pg");
 const settings = require("./settings");
-const[name] = process.argv.slice(2);
-if(!name) {
+const [name] = process.argv.slice(2);
+if (!name) {
   console.log("Please provide a query")
   return process.exit(1);
 }
 
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
 });
 
 
@@ -21,10 +21,10 @@ const client = new pg.Client({
 function findPerson(rows) {
 
   console.log(`Found ${rows.length} person(s) by the name '${name}':`);
- if(rows.length === 0 ) {
+  if (rows.length === 0) {
     console.log("Try another search")
   }
-  rows.forEach( function(row) {
+  rows.forEach(function(row) {
     const id = row.id;
     const firstName = row.first_name;
     const lastName = row.last_name;
@@ -39,15 +39,16 @@ client.connect((err) => {
     return console.error("Error connecting", err);
   }
   console.log("Searching ...");
-  client.query(`SELECT *
+
+  const query = `SELECT *
     FROM famous_people
-    WHERE first_name LIKE $1::text`, [name], (err, result) => {
+    WHERE first_name LIKE $1::text`;
+  const params = [name];
+  client.query(query, params, (err, result) => {
     if (err) {
       return console.error("Please try another search", err);
     }
-
     findPerson(result.rows);
     client.end();
   });
 });
-
